422ca8247341d9d86eecfae07e7e7af30c6fdceb
Tanishq Soni <tanishqsoni992857@gmail.com>
2021-11-14 20:30:52 +0530

    first commit

---
+++ first_Complex_code.py
@@ -0,0 +1,6 @@
+name = input("What's your name : ")
+if name == 'Tanishq':
+    for i in range(5):
+        print(f"Hello Tanishq {i}")
+else:
+    print("who are you")
---
+++ first_assignment.py
@@ -0,0 +1,9 @@
+""" 
+Q1    write a code o print 
+        Hi there my name is Tanishq Tanishq Tanishq Tanishq Tanishq Tanishq Tanishq Tanishq Tanishq Tanishq 
+        you are in D:\programes\python\new
+"""
+
+print(" Hi there my name is")
+print(" Tanishq"*10)
+print(r"you are in D:\programes\python\new")
